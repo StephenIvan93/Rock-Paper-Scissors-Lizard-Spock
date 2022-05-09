a7ef67088c424007f073e08db894c02303051139
StephenIvan93 <stephensheppard93@gmail.com>
2022-05-09 23:53:17 +0000

    Cleaned up CSS and completed comparision checks against each token

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 
     // Player token function
 function pickPlayerToken(token) {
@@ -13,18 +15,18 @@ function pickPlayerToken(token) {
 
     //Set the players choice
     if(token === "rock")
-        document.getElementById("playerChoice").src = "/assets/images/rock.png";
+        document.getElementById("playerChoice").src = "assets/images/rock.png";
     else if (token === "paper")
-        document.getElementById("playerChoice").src = "/assets/images/paper.jpg";
+        document.getElementById("playerChoice").src = "assets/images/paper.jpg";
     else if (token === "scissors")
-        document.getElementById("playerChoice").src = "/assets/images/scissors.jpg";
+        document.getElementById("playerChoice").src = "assets/images/scissors.jpg";
         else if (token === "lizard")
-        document.getElementById("playerChoice").src = "/assets/images/lizard.png";
+        document.getElementById("playerChoice").src = "assets/images/lizard.png";
         else if (token === "spock")
-        document.getElementById("playerChoice").src = "/assets/images/spock.jpg";
+        document.getElementById("playerChoice").src = "assets/images/spock.jpg";
 
         pickComputerToken(token);
-  }
+  } 
 
     // Computer Token Function
 function pickComputerToken(token) {
@@ -33,49 +35,126 @@ function pickComputerToken(token) {
 
     //Set the Computer choice
     if(computerToken === "rock")
-        document.getElementById("computerChoice").src = "/assets/images/rock.png";
+        document.getElementById("computerChoice").src = "assets/images/rock.png";
     else if (computerToken === "paper")
-        document.getElementById("computerChoice").src = "/assets/images/paper.jpg";
+        document.getElementById("computerChoice").src = "assets/images/paper.jpg";
     else if (computerToken === "scissors")
-        document.getElementById("computerChoice").src = "/assets/images/scissors.jpg";
+        document.getElementById("computerChoice").src = "assets/images/scissors.jpg";
         else if (computerToken === "lizard")
-        document.getElementById("computerChoice").src = "/assets/images/lizard.png";
+        document.getElementById("computerChoice").src = "assets/images/lizard.png";
         else if (computerToken === "spock")
-        document.getElementById("computerChoice").src = "/assets/images/spock.jpg";
+        document.getElementById("computerChoice").src = "assets/images/spock.jpg";
 
-        ReferenceError(token, computerToken);
-}
+        referee(token, computerToken);
+}   
 
 // Check if Player is a Winner or Loser Function
+function referee(token, computerToken) {
+    // Rock vs Computer
+    if (token == "rock" && computerToken == "paper") {
+        setDecision("You Loose!");
+        setComputerScore(computerScore+1);
+    } if (token == "rock" && computerToken == "spock") {
+        setDecision("You Loose!");
+        setComputerScore(computerScore+1);
+    } if (token == "rock" && computerToken == "scissors") {
+        setDecision("You Win!");
+        setPlayerScore(playerScore+1);
+    } if (token == "rock" && computerToken == "Lizard") {
+        setDecision("YOU Win!");
+        setPlayerScore(playerScore+1);
+    } if (token == "rock" && computerToken == "rock") {
+        setDecision("It's a Tie!");
+    }
+    // Paper vs Computer
+    if (token == "paper" && computerToken == "rock") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "paper" && computerToken == "paper") {
+        setDecision("It's a Tie!")
+    } if (token == "paper" && computerToken == "scissors") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "paper" && computerToken == "lizard") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "paper" && computerToken == "spock") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    }
+    // Scissors vs Computer
+    if (token == "scissors" && computerToken == "rock") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "scissors" && computerToken == "paper") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "scissors" && computerToken == "scissors") {
+        setDecision("It's a Tie!")
+    } if (token == "scissors" && computerToken == "lizard") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "scissors" && computerToken == "spock") {
+        setDecision("You Win!")
+        setComputerScore(computerScore+1);
+    }
+    // Lizard vs Computer
+    if (token == "lizard" && computerToken == "rock") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "lizard" && computerToken == "paper") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "lizard" && computerToken == "scissors") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "lizard" && computerToken == "lizard") {
+        setDecision("It's a Tie!")
+    } if (token == "lizard" && computerToken == "spock") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    }
+    // Spock vs Computer
+    if (token == "spock" && computerToken == "rock") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "spock" && computerToken == "paper") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "spock" && computerToken == "scissors") {
+        setDecision("You Win!")
+        setPlayerScore(playerScore+1);
+    } if (token == "spock" && computerToken == "lizard") {
+        setDecision("You Loose!")
+        setComputerScore(computerScore+1);
+    } if (token == "spock" && computerToken == "spock") {
+        setDecision("It's a Tie!")
+    }
+} 
 
-function referee(playerToken, computerToken) {
-    if (playerToken == "rock" && computerToken == "paper") {
-        setDecision("You Loose, Paper covers Rock ")
-    } 
+// Output whether Player is Winner, Loser or if it's a tie 
+function setDecision(decision) {
+    document.querySelector(".decision h2").innerText = decision;
+} 
 
-    
+// Increment player Score by 1
+function setPlayerScore(playerNewScore) {
+    playerScore = playerNewScore;
+    document.querySelector(".playerScore h2").innerText = playerNewScore;
 }
 
-function setDecision(decision) {
-    document.querySelector(".decision h1").innerText = decision;
-  }
-  
+// Increment Computer score by 1
+function setComputerScore(computerNewScore) {
+    computerScore = computerNewScore;
+    document.querySelector(".computerScore h2").innerText = computerNewScore;
+}
+
+// Reset Game
 function restartGame() {
     let contest = document.querySelector(".contest");
     contest. style.display = "none";
   
-    let hands = document.querySelector(".tokens");
+    let tokens = document.querySelector(".tokens");
     tokens.style.display = "flex";
-  }
-/*
-
+} 
   
-  const setDecision = (decision) => {
-    document.querySelector(".decision h1").innerText = decision;
-  }
-  
-  const setScore = (newScore) => {
-    SCORE = newScore;
-    document.querySelector(".score h1").innerText = newScore;
-  }
-  */
